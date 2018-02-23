d30bbc59204caefd27635e85a511e952eda48989
Matt Niehoff <matt.j.niehoff@gmail.com>
2018-02-23 10:22:42 -0600

    Creates and wires up the RecyclerView for our book list.

--- app/src/main/java/org/nypl/pspdfkitandroidexample/MainActivity.kt
+++ app/src/main/java/org/nypl/pspdfkitandroidexample/MainActivity.kt
@@ -2,11 +2,18 @@ package org.nypl.pspdfkitandroidexample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var layoutManager: LinearLayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        layoutManager = LinearLayoutManager(this)
+        rv_book_list.layoutManager = layoutManager
     }
 }
